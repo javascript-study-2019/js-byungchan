6aebac66acb5616f382a5177c2f18d820142404b
Byungchan-Park <57528803+Byungchan-Park@users.noreply.github.com>
2019-11-27 00:08:34 +0900

    Create 2558.js

---
+++ BOJ/01/2558.js
@@ -0,0 +1,5 @@
+var A = "472", B = "385";
+console.log(Number(A) * Number(B[2]));
+console.log(Number(A) * Number(B[1]));
+console.log(Number(A) * Number(B[0]));
+console.log(Number(A) * Number(B));
