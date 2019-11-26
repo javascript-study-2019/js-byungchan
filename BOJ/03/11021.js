c5cb31e31269c70fb0cb9fe0f8d32d6b27c8f4e9
Byungchan-Park <57528803+Byungchan-Park@users.noreply.github.com>
2019-11-27 00:14:33 +0900

    Create 11021.js

---
+++ BOJ/03/11021.js
@@ -0,0 +1,8 @@
+var number = prompt("몇 개를 출력하고 싶은가요?");
+var sum = 0;
+for(var i = 1; i < Number(number) + 1; i++) {
+    var numA = prompt("두 숫자 중 하나");
+    var numB = prompt("나머지 숫자");
+    sum = Number(numA) + Number(numB);
+    console.log('Case #' + i + ': ' + sum);
+}
