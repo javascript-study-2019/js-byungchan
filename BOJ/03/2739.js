0bf055221ed1c8de4ce047d73ed00a0ce1418eca
Byungchan-Park <57528803+Byungchan-Park@users.noreply.github.com>
2019-11-27 00:11:38 +0900

    Create 2739.js

---
+++ BOJ/03/2739.js
@@ -0,0 +1,5 @@
+var N = prompt('몇 단을 출력할까요?'); // 출력받을 단의 숫자 N을 입력받음.
+for(var i = 1; i < 10; i++) {   // 곱셈 9번 반복
+  ans = N * i;
+  console.log(String(N) + '*' + String(i) + '=' + String(ans)); 
+}
